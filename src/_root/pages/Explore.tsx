d29092c3fc0d357c4ffcf3eea263ddebf1ba9696
SagirGarba <sagir40me@gmail.com>
2024-04-15 12:38:29 +0100

    Implemented Searching Post, with some bugs to fix.

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -64,7 +64,10 @@ const Explore = () => {
       </div>
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
-          <SearchResults isSearchFetching={isSearchFetching} searchedPosts={searchedPosts/>
+          <SearchResults
+            isSearchFetching={isSearchFetching}
+            searchedPosts={searchedPosts}
+          />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of Posts</p>
         ) : (
@@ -73,6 +76,12 @@ const Explore = () => {
           ))
         )}
       </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,7 +1,26 @@
+import { Models } from "appwrite";
 import React from "react";
+import Loader from "./Loader";
+import GridPostList from "./GridPostList";
 
-const SearchResults = () => {
-  return <div>SearchResults</div>;
+type searchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document;
+};
+
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts,
+}: searchResultsProps) => {
+  if (isSearchFetching) return <Loader />;
+
+  if (searchedPosts && searchedPosts.documents.length > 0) {
+    return <GridPostList posts={searchedPosts.documents} />;
+  }
+
+  return (
+    <p className="text-light-4 mt-10 text-center w-full">No results found</p>
+  );
 };
 
 export default SearchResults;
